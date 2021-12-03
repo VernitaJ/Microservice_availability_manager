57cebd4cc7769b983dc0da4dd12cb5439a46a7e3
jens Sjödin <jeens93@gmail.com>
2021-12-03 16:36:56 +0100

    Configure db and design schema Co-authored-by: axelindma <axelindma@student.chalmers.se>

---
+++ db.js
@@ -0,0 +1,43 @@
+require("dotenv").config();
+const { MongoClient } = require("mongodb");
+const mongoURL = `mongodb://mongodb:${process.env.MONGODB_DOCKER_PORT}`;
+const client = new MongoClient(mongoURL);
+
+// Use create time slots result as parameter
+async function insertMany(timeslots) {
+  await client.connect();
+  console.log("Connected successfully to server");
+
+  const collection = client.db("availability").collection("timeslot");
+  const insertResult = await collection.insertMany([timeslots]);
+  console.log("Inserted documents =>", insertResult);
+
+  return "Success";
+}
+
+function insertTimeSlots(timeslots) {
+  insertMany(timeslots)
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+}
+
+// use dentistId as parameter
+async function find(dentistId) {
+  await client.connect();
+  console.log("Connected successfully to server");
+
+  const collection = client.db("availability").collection("timeslot");
+  const findResult = await collection.find({ dentistId: dentistId }).toArray();
+  console.log("Found documents =>", findResult);
+  return "Success";
+}
+
+function findTimeSlotByDentistId(dentistId) {
+  find(dentistId)
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+}
+
+module.exports = { insertTimeSlots, findTimeSlotByDentistId };
--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const { runMqtt } = require("./mqtt");
+const { insertTimeSlots, findTimeSlotByDentistId } = require("./db");
 
-runMqtt();
+//runMqtt();
+const timeslots = { a: 1, a: 3, a: 4 };
+insertTimeSlots(timeslots);
+findTimeSlotByDentistId();
 
 // Start mqtt
 // Start mongodb
-// Run create timeslot script
+// Run create timeslots script
